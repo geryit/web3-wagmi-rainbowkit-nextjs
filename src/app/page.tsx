0de19292ccdfd7a40fbbc65a60b6bd480ac7ca04
Goksel Eryigit <goksel@carbonhealth.com>
2024-05-16 19:05:25 +0300

    `chore: Remove unused code and dependencies`

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,5 @@
 "use client";
 
-import Image from "next/image";
-import Profile from "./components/profile";
 import { ConnectButton } from "@rainbow-me/rainbowkit";
 import Send from "./components/send-transaction";
 
@@ -15,39 +13,6 @@ export default function Home() {
       <div>
         <Send />
       </div>
-
-      <div className="flex items-center gap-8 flex-wrap justify-center">
-        <div>
-          <Image
-            className=""
-            src="/rainbow.svg"
-            alt="Rainbowkit Logo"
-            width={60}
-            height={60}
-            priority
-          />
-          <span className="text-xl">RainbowKit</span>
-        </div>
-        <span className="text-4xl"> + </span>
-        <div>
-          <span className="text-6xl">Wagmi</span>
-        </div>
-
-        <span className="text-4xl"> + </span>
-
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      {/* Implement buy donut form to use eth */}
-
-      <Profile />
     </main>
   );
 }
